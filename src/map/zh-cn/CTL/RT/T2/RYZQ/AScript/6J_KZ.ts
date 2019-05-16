93c3c9d81a372f5de7ea17a996bd42e48d463e05
jialiang.li <80201288@qq.com>
2019-05-16 16:30:08 +0800

    修复bug

--- src/map/zh-cn/CTL/RT/T2/RYZQ/AScript/6J_KZ.ts
+++ src/map/zh-cn/CTL/RT/T2/RYZQ/AScript/6J_KZ.ts
@@ -1,12 +1,12 @@
 import { OpenCloseField } from "../../../../../../../meta/RT/meta";
 import { Map_ZH_CN } from "../../../../../ZH_CN";
 import { Map_CTL_RT_T2_RYZQ_6J } from "./6J";
-import { CTL_RT_T2_RYZQ_Ts } from "../../../../../../../devices/CTL/RT/T2/AScript/RYZQ";
 import { CountShowField } from "../../../../../../../meta/CountShowField";
 import { INumberIndex } from "../../../../../../../entities/IIndex";
 
 export class Map_CTL_RT_T2_RYZQ_6JKZ extends Map_CTL_RT_T2_RYZQ_6J{
     //6电极压力控制器2端火
+    static readonly KEY_POINT_YA_LI_STATUS='ba_yali_status'
     static KZQ_YaLiStatus: INumberIndex = {        
         0: '高压',
         1: '中压',
@@ -15,7 +15,7 @@ export class Map_CTL_RT_T2_RYZQ_6JKZ extends Map_CTL_RT_T2_RYZQ_6J{
     }
     constructor(){
         super();
-        this.addPoint(new CountShowField(Map_CTL_RT_T2_RYZQ_6JKZ.KEY_BASE,CTL_RT_T2_RYZQ_Ts.KEY_POINT_YA_LI_STATUS, "压力状态",''));
+        this.addPoint(new CountShowField(Map_CTL_RT_T2_RYZQ_6JKZ.KEY_BASE,Map_CTL_RT_T2_RYZQ_6JKZ.KEY_POINT_YA_LI_STATUS, "压力状态",''));
         this.addPoint(new OpenCloseField("oc_ranshaoqifuhe_zengda", 9, 2, "燃烧器负荷增大调节", 1, Map_ZH_CN.coms_open_close));
         this.addPoint(new OpenCloseField("oc_fuhetiaojie", 5, 2, "负荷调节", 7, Map_ZH_CN.coms_open_close));
         this.addPoint(new OpenCloseField("oc_qitingkongzhi", 5, 2, "启停控制", 8, Map_ZH_CN.coms_open_close));
